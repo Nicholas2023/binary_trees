39944db2b5e1cfc483f3a4e03e304deec7085704
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-08-03 06:15:03 +0300

    tas114

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -2,7 +2,7 @@
 
 /**
  * bst_remove - Removes a node from a Binary Search Tree (BST).
- * @root: Pointer to the root node of the tree where the 
+ * @root: Pointer to the root node of the tree where the
  * node will be removed.
  * @value: Value to remove from the tree.
  *
@@ -11,6 +11,8 @@
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
+	bst_t *temp;
+
 	if (root == NULL)
 		return (NULL);
 
@@ -23,19 +25,19 @@ bst_t *bst_remove(bst_t *root, int value)
 		/* Node with one child or no child */
 		if (root->left == NULL)
 		{
-			bst_t *temp = root->right;
+			temp = root->right;
 			free(root);
 			return (temp);
 		}
 		else if (root->right == NULL)
 		{
-			bst_t *temp = root->left;
+			temp = root->left;
 			free(root);
 			return (temp);
 		}
 
 		/* Node with two children, find the in-order successor */
-		bst_t *temp = root->right;
+		temp = root->right;
 		while (temp->left != NULL)
 			temp = temp->left;
 
