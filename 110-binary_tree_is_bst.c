a8be289295867518837f53150131692497a86707
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-08-03 05:40:00 +0300

    task110

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,46 +1,35 @@
 #include "binary_trees.h"
 #include <limits.h>
 
-int is_bst_recursive(const binary_tree_t *tree, int *prev);
-
 /**
- * binary_tree_is_bst - Checks if a binary tree is a
- * valid Binary Search Tree (BST).
+ * is_bst_helper - Checks if a binary tree is a valid binary search tree.
  * @tree: A pointer to the root node of the tree to check.
+ * @lo: The value of the smallest node visited thus far.
+ * @hi: The value of the largest node visited this far.
  *
- * Return: 1 if tree is a valid BST, otherwise 0.
+ * Return: If the tree is a valid BST, 1, otherwise, 0.
  */
-int binary_tree_is_bst(const binary_tree_t *tree)
+int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
 {
-	int prev = INT_MIN; /* Minimum possible integer */
-
-	return (is_bst_recursive(tree, &prev));
+	if (tree != NULL)
+	{
+		if (tree->n < lo || tree->n > hi)
+			return (0);
+		return (is_bst_helper(tree->left, lo, tree->n - 1) &&
+			is_bst_helper(tree->right, tree->n + 1, hi));
+	}
+	return (1);
 }
 
 /**
- * is_bst_recursive - Helper function to check if a binary
- * tree is a valid BST recursively.
- * @tree: A pointer to the current node in the recursive call.
- * @prev: A pointer to the previous node's value in the in-order trav.
+ * binary_tree_is_bst - Checks if a binary tree is a valid binary search tree.
+ * @tree: A pointer to the root node of the tree to check.
  *
- * Return: 1 if tree is a valid BST, otherwise 0.
+ * Return: 1 if tree is a valid BST, and 0 otherwise
  */
-int is_bst_recursive(const binary_tree_t *tree, int *prev)
+int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (1);
-
-	/* Check left subtree */
-	if (!is_bst_recursive(tree->left, prev))
 		return (0);
-
-	/* Check current node's value against previous node's value */
-	if (tree->n <= *prev)
-		return (0);
-
-	/* Update previous node's value to the current node's value */
-	*prev = tree->n;
-
-	/* Check right subtree */
-	return (is_bst_recursive(tree->right, prev));
+	return (is_bst_helper(tree, INT_MIN, INT_MAX));
 }
